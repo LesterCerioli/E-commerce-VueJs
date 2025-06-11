fab69fb0cacccd96e9df5632503c938454d9bbdc
LesterCerioli <lesterlucasit@hotmail.com>
2025-06-11 13:24:53 -0400

    Initial repository

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.jsx'; // Ensure it points to App.jsx
-import router from './router'; // Import the router
-
-// Create the Vue app, use the router, and mount it
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
